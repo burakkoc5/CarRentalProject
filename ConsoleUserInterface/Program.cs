08d6ff9774e930358a8112177cf1e3ecb0a5bff4
Burak Koç <burak.koc@tedu.edu.tr>
2021-02-11 14:57:17 +0300

    10.HaftaOdev

--- ConsoleUserInterface/Program.cs
+++ ConsoleUserInterface/Program.cs
@@ -24,12 +24,12 @@ namespace ConsoleUserInterface
         private static void CarManagerTest()
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (CarDetailDto car in carManager.GetCarDetails())
+            foreach (CarDetailDto car in carManager.GetCarDetails().Data)
             {
                 Console.WriteLine("{0} model, {1} {2} Günlük Ücret : {3} / Renk : {4}", car.ModelYear, car.BrandName, car.Description, car.DailyPrice, car.ColorName);
             }
             carManager.Add(new Car { BrandId = 4, DailyPrice = 200, Description = "Auris", ColorId = 2, ModelYear = 2018 });
-            Console.WriteLine(carManager.GetCarById(5).Description);
+            Console.WriteLine(carManager.GetCarById(5).Data.Description);
         }
     }
 }
